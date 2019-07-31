d80de4248059cced207f2c68555605c28791f17f
Vincent Wetzel <vincentwetzel3@gmail.com>
2019-07-31 16:53:55 -0600

    v1.2
    Added an option to open results.csv at the conclusion of runtime.

--- Lords_Mobile_Boost_Calculator.py
+++ Lords_Mobile_Boost_Calculator.py
@@ -1,45 +1,54 @@
 from datetime import datetime
 import csv
+import os
 
 
 def main():
     print_section("Welcome to Vincent's Speed Up Calculator for Lord's Mobile!", '*')
 
-    speed_up_3m = (get_int_input("\nEnter the number of Speed Up (3 m) you have:") * 3)
-    speed_up_5m = (get_int_input("Enter the number of Speed Up (5 m) you have:") * 5)
-    speed_up_10m = (get_int_input("Enter the number of Speed Up (10 m) you have:") * 10)
-    speed_up_15m = (get_int_input("Enter the number of Speed Up (15 m) you have:") * 15)
-    speed_up_30m = (get_int_input("Enter the number of Speed Up (30 m) you have:") * 30)
-    speed_up_60m = (get_int_input("Enter the number of Speed Up (60 m) you have:") * 60)
-    speed_up_3h = (get_int_input("Enter the number of Speed Up (3 h) you have:") * 180)
-    speed_up_8h = (get_int_input("Enter the number of Speed Up (8 h) you have:") * 480)
-    speed_up_24h = (get_int_input("Enter the number of Speed Up (24 h) you have:") * 1440)
-    sum_speed_ups = speed_up_3m + speed_up_5m + speed_up_10m + speed_up_15m + speed_up_30m + speed_up_60m + speed_up_3h + speed_up_8h + speed_up_24h
+    speed_up_3m = get_int_input("\nEnter the number of Speed Up (3 m) you have:")
+    speed_up_5m = get_int_input("Enter the number of Speed Up (5 m) you have:")
+    speed_up_10m = get_int_input("Enter the number of Speed Up (10 m) you have:")
+    speed_up_15m = get_int_input("Enter the number of Speed Up (15 m) you have:")
+    speed_up_30m = get_int_input("Enter the number of Speed Up (30 m) you have:")
+    speed_up_60m = get_int_input("Enter the number of Speed Up (60 m) you have:")
+    speed_up_3h = get_int_input("Enter the number of Speed Up (3 h) you have:")
+    speed_up_8h = get_int_input("Enter the number of Speed Up (8 h) you have:")
+    speed_up_24h = get_int_input("Enter the number of Speed Up (24 h) you have:")
+    sum_speed_ups = (speed_up_3m * 3) + (speed_up_5m * 5) + (speed_up_10m * 10) + (speed_up_15m * 15) + (
+            speed_up_30m * 30) + (speed_up_60m * 60) + (speed_up_3h * 180) + (speed_up_8h * 480) + (
+                            speed_up_24h * 1440)
 
-    speed_up_research_5m = (get_int_input("\n\nEnter the number of Speed Up Research (5 m) you have:") * 5)
-    speed_up_research_10m = (get_int_input("Enter the number of Speed Up Research (10 m) you have:") * 10)
-    speed_up_research_15m = (get_int_input("Enter the number of Speed Up Research (15 m) you have:") * 15)
-    speed_up_research_30m = (get_int_input("Enter the number of Speed Up Research (30 m) you have:") * 30)
-    speed_up_research_60m = (get_int_input("Enter the number of Speed Up Research (60 m) you have:") * 60)
-    speed_up_research_3h = (get_int_input("Enter the number of Speed Up Research (3 h) you have:") * 180)
-    speed_up_research_8h = (get_int_input("Enter the number of Speed Up Research (8 h) you have:") * 480)
-    speed_up_research_24h = (get_int_input("Enter the number of Speed Up Research (24 h) you have:") * 1440)
-    sum_speed_up_research = speed_up_research_5m + speed_up_research_10m + speed_up_research_15m + speed_up_research_30m + speed_up_research_60m + speed_up_research_3h + speed_up_research_8h + speed_up_research_24h
+    speed_up_research_5m = get_int_input("\n\nEnter the number of Speed Up Research (5 m) you have:")
+    speed_up_research_10m = get_int_input("Enter the number of Speed Up Research (10 m) you have:")
+    speed_up_research_15m = get_int_input("Enter the number of Speed Up Research (15 m) you have:")
+    speed_up_research_30m = get_int_input("Enter the number of Speed Up Research (30 m) you have:")
+    speed_up_research_60m = get_int_input("Enter the number of Speed Up Research (60 m) you have:")
+    speed_up_research_3h = get_int_input("Enter the number of Speed Up Research (3 h) you have:")
+    speed_up_research_8h = get_int_input("Enter the number of Speed Up Research (8 h) you have:")
+    speed_up_research_24h = get_int_input("Enter the number of Speed Up Research (24 h) you have:")
+    sum_speed_up_research = (speed_up_research_5m * 5) + (speed_up_research_10m * 10) + (speed_up_research_15m * 15) + (
+            speed_up_research_30m * 30) + (speed_up_research_60m * 60) + (speed_up_research_3h * 180) + (
+                                    speed_up_research_8h * 480) + (speed_up_research_24h * 1440)
 
-    speed_up_merging_15m = (get_int_input("\n\nEnter the number of Speed Up Merging (15 m) you have:") * 15)
-    speed_up_merging_60m = (get_int_input("Enter the number of Speed Up Merging(60 m) you have:") * 60)
-    speed_up_merging_3h = (get_int_input("Enter the number of Speed Up Merging (3 h) you have:") * 180)
-    speed_up_merging_8h = (get_int_input("Enter the number of Speed Up Merging (8 h) you have:") * 480)
-    sum_speed_up_merging = speed_up_merging_15m + speed_up_merging_60m + speed_up_merging_3h + speed_up_merging_8h
+    speed_up_merging_15m = get_int_input("\n\nEnter the number of Speed Up Merging (15 m) you have:")
+    speed_up_merging_60m = get_int_input("Enter the number of Speed Up Merging(60 m) you have:")
+    speed_up_merging_3h = get_int_input("Enter the number of Speed Up Merging (3 h) you have:")
+    speed_up_merging_8h = get_int_input("Enter the number of Speed Up Merging (8 h) you have:")
+    sum_speed_up_merging = (speed_up_merging_15m * 15) + (speed_up_merging_60m * 60) + (speed_up_merging_3h * 180) + (
+            speed_up_merging_8h * 480)
 
     print("\n\nTotal time from speed ups: " + format_minutes_to_time(sum_speed_ups))
     print("Total time from speed up research: " + format_minutes_to_time(sum_speed_up_research))
     print("Total time from speed up merging: " + format_minutes_to_time(sum_speed_up_merging))
 
+    note = input("\n\nEnter a note for this data set: ")
+
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     fieldnames_for_csv = [
         "Date/Time",
+        "Notes",
         "Speed_Up_Total",
         "Speed_Up_Research_Total",
         "Speed_Up_Merging_Total",
@@ -63,35 +72,49 @@ def main():
         "Speed_Up_Merging_15m",
         "Speed_Up_Merging_60m",
         "Speed_Up_Merging_3h",
-        "Speed_Up_Merging_8h"]
+        "Speed_Up_Merging_8h",
+    ]
+
     with open("results.csv", 'a', newline='') as outfile:  # output csv file
         writer = csv.DictWriter(outfile, fieldnames=fieldnames_for_csv)
         writer.writerow({"Date/Time": current_time,
-                         "Speed_Up_Total": sum_speed_ups,
-                         "Speed_Up_Research_Total": sum_speed_up_research,
-                         "Speed_Up_Merging_Total": sum_speed_up_merging,
-                         "Speed_Up_3m": speed_up_3m,
-                         "Speed_Up_5m": speed_up_5m,
-                         "Speed_Up_10m": speed_up_10m,
-                         "Speed_Up_15m": speed_up_15m,
-                         "Speed_Up_30m": speed_up_30m,
-                         "Speed_Up_60m": speed_up_60m,
-                         "Speed_Up_3h": speed_up_3h,
-                         "Speed_Up_8h": speed_up_8h,
-                         "Speed_Up_24h": speed_up_24h,
-                         "Speed_Up_Research_5m": speed_up_research_5m,
-                         "Speed_Up_Research_10m": speed_up_research_10m,
-                         "Speed_Up_Research_15m": speed_up_research_15m,
-                         "Speed_Up_Research_30m": speed_up_research_30m,
-                         "Speed_Up_Research_60m": speed_up_research_60m,
-                         "Speed_Up_Research_3h": speed_up_research_3h,
-                         "Speed_Up_Research_8h": speed_up_research_8h,
-                         "Speed_Up_Research_24h": speed_up_research_24h,
-                         "Speed_Up_Merging_15m": speed_up_merging_15m,
-                         "Speed_Up_Merging_60m": speed_up_merging_60m,
-                         "Speed_Up_Merging_3h": speed_up_merging_3h,
-                         "Speed_Up_Merging_8h": speed_up_merging_8h
+                         "Notes": note,
+                         "Speed_Up_Total": format_minutes_to_time(sum_speed_ups),
+                         "Speed_Up_Research_Total": format_minutes_to_time(sum_speed_up_research),
+                         "Speed_Up_Merging_Total": format_minutes_to_time(sum_speed_up_merging),
+                         "Speed_Up_3m": format_minutes_to_time(speed_up_3m * 3),
+                         "Speed_Up_5m": format_minutes_to_time(speed_up_5m * 5),
+                         "Speed_Up_10m": format_minutes_to_time(speed_up_10m * 10),
+                         "Speed_Up_15m": format_minutes_to_time(speed_up_15m * 15),
+                         "Speed_Up_30m": format_minutes_to_time(speed_up_30m * 30),
+                         "Speed_Up_60m": format_minutes_to_time(speed_up_60m * 60),
+                         "Speed_Up_3h": format_minutes_to_time(speed_up_3h * 180),
+                         "Speed_Up_8h": format_minutes_to_time(speed_up_8h * 480),
+                         "Speed_Up_24h": format_minutes_to_time(speed_up_24h * 1440),
+                         "Speed_Up_Research_5m": format_minutes_to_time(speed_up_research_5m * 5),
+                         "Speed_Up_Research_10m": format_minutes_to_time(speed_up_research_10m * 10),
+                         "Speed_Up_Research_15m": format_minutes_to_time(speed_up_research_15m * 15),
+                         "Speed_Up_Research_30m": format_minutes_to_time(speed_up_research_30m * 30),
+                         "Speed_Up_Research_60m": format_minutes_to_time(speed_up_research_60m * 60),
+                         "Speed_Up_Research_3h": format_minutes_to_time(speed_up_research_3h * 180),
+                         "Speed_Up_Research_8h": format_minutes_to_time(speed_up_research_8h * 480),
+                         "Speed_Up_Research_24h": format_minutes_to_time(speed_up_research_24h * 1440),
+                         "Speed_Up_Merging_15m": format_minutes_to_time(speed_up_merging_15m * 15),
+                         "Speed_Up_Merging_60m": format_minutes_to_time(speed_up_merging_60m * 60),
+                         "Speed_Up_Merging_3h": format_minutes_to_time(speed_up_merging_3h * 180),
+                         "Speed_Up_Merging_8h": format_minutes_to_time(speed_up_merging_8h * 480)
                          })
+
+    while True:
+        user_input = input("Do you want to open results.csv? (y/n):").lower()
+        if user_input == 'y' or user_input == "yes":
+            os.startfile("results.csv")
+            break
+        elif user_input == 'n' or user_input == "no":
+            break
+        else:
+            print("That didn't work. Please try again.")
+    # Done!
 
 
 def get_int_input(query_str):
@@ -116,13 +139,14 @@ def format_minutes_to_time(minutes):
 
     hours = minutes // 60
     minutes = minutes % 60
-
+    if hours == 0:
+        return str(minutes) + " Minutes"
     if hours < 24:
         return str(hours) + " Hour, " + str(minutes) + " Minutes"
     else:
         days = hours // 24
         hours = hours % 24
-        return str(days) + " Days, " + str(hours) + " Hour, " + str(minutes) + " Minutes"
+        return str(days) + " Days, " + str(hours) + " Hours, " + str(minutes) + " Minutes"
 
 
 def print_section(section_title, symbol):
